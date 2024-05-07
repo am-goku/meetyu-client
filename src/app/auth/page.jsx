90a3744e0432994234cfd911f7f3cb328436df9b
Gokul Krishna <amkrishna2483@gmail.com>
2024-05-07 18:16:01 +0530

    1. Added logo and icon.
    2. Page: Separated login and signup and managed it in the same page component along with product icon. Now both are combined in "/auth" route.
    
    3. Login: Separated login component
    
    4. Signup: Separated signup component
    
    5. Styles: Added a common css for Login.hsx and Signup.jsx. Also anothe css for Page to amnage animation
    
    6. Removed Old page file along with its stylesheet.

---
+++ src/app/auth/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+import React from "react"
+import "./style.css"
+import Login from '../components/auth/Login';
+import Signup from "../components/auth/Signup";
+
+
+function page() {
+
+  const [signup, setSignup] = React.useState(false)
+
+
+  const handleSignup = (data) => {
+    console.log('====================================');
+    console.log("user data", data);
+    console.log('====================================');
+  }
+
+
+  return (
+    <div className="login flex-1 flex items-center justify-center"   >
+      <div className='centerDiv flex w-full justify-center p-5'>
+        {
+          signup ?
+            <>
+              <Signup setSignup={setSignup} handleSignup={handleSignup} />
+              <div className='icnContainer mainContainer hidden md:flex' />
+            </>
+            :
+            <>
+              <div className='icnContainer mainContainer hidden md:flex' />
+              <Login setSignup={setSignup} />
+            </>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default page
